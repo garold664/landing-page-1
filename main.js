5f533c81e3bb68ce9545301016b2ff019e550c29
garold664 <garold664@gmail.com>
2024-05-08 21:37:27 +0300

    programmed the burger button'

---
+++ main.js
@@ -0,0 +1,10 @@
+const navToggle = document.querySelector('.mobile-nav-toggle');
+const primaryNav = document.querySelector('.primary-navigation');
+
+navToggle.addEventListener('click', () => {
+  // const visibility = nav.getAttribute("data-visible");
+  primaryNav.hasAttribute('data-visible')
+    ? navToggle.setAttribute('aria-expanded', false)
+    : navToggle.setAttribute('aria-expanded', true);
+  primaryNav.toggleAttribute('data-visible');
+});
